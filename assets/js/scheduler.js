49c708514a1855d84524e7e0718cd9b953964680
Jennifer Cuenca <>
2018-03-11 15:23:50 -0400

    added reset.css // added boiler plate code to index.html including link to firebase// added initialization code to scheduler.js file

---
+++ assets/js/scheduler.js
@@ -0,0 +1,23 @@
+$(document).on('ready', function() { 
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyAdolw4zVwZr5YfUjBU689M4gkLwuLDfJo",
+    authDomain: "train-scheduler-48b81.firebaseapp.com",
+    databaseURL: "https://train-scheduler-48b81.firebaseio.com",
+    projectId: "train-scheduler-48b81",
+    storageBucket: "",
+    messagingSenderId: "827412972876"
+  };
+  
+  firebase.initializeApp(config);
+
+
+
+
+
+
+
+});
+
+
