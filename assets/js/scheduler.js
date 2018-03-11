c5a77c8508290314438a93041be671fb26021056
Jennifer Cuenca <>
2018-03-11 16:05:32 -0400

    started coding function to obtain train info from form

--- assets/js/scheduler.js
+++ assets/js/scheduler.js
@@ -9,15 +9,30 @@ var config = {
     storageBucket: "",
     messagingSenderId: "827412972876"
   };
-  
+
   firebase.initializeApp(config);
 
+  $("#addTrainBtn").on("click", function(){
+        event.preventDefault(); // prevents form default from reloading page
+
+        //Grabs input from form
+        const newTrain = {
+            trainName: $("#train-name-input").val().trim(),
+            destination: $("#destination-input").val().trim(),
+            frequency: $("#frequency-input").val().trim(),
+            firstTime: $("#first-time-input").val().trim(),
+        }
+
+        //console log everything
+        console.log(newTrain.trainName)
+        console.log(newTrain.destination)
+        console.log(newTrain.frequency)
+        console.log(newTrain.firstTime)
+
+        //pushes train data to firebase
+        database.ref().push(newTrain);
 
 
+    });//end submitBtn onClick
 
-
-
-
-});
-
-
+}); //end of onReady Function
